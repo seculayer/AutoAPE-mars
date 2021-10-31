504e5ab69dc0f2ccdb30e62e2685bd136d0e90ce
manki baek <manki.baek@seculayer.com>
2021-11-01 07:17:54 +0900

    mars basic

--- mars.sh
+++ mars.sh
@@ -6,16 +6,16 @@
 # Powered by Seculayer © 2021 Service Model Team, R&D Center.
 ######################################################################################
 
-#APP_PATH=/eyeCloudAI/app/ape
-#
-#MARS_LIB_PATH=$APP_PATH/mars/lib
-#MARS_PATH=$APP_PATH/mars
-#####
-#export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
-#export CUDA_HOME=/usr/local/cuda
-#export PYTHONPATH=$MARS_LIB_PATH:$MARS_PATH
-#
-#KEY=${1}
-#
-#/usr/local/bin/python3.7 -m mars.MLAlgRecommender ${KEY}
-echo "hello mars"
+APP_PATH=/eyeCloudAI/app/ape
+
+MARS_LIB_PATH=$APP_PATH/mars/lib
+MARS_PATH=$APP_PATH/mars
+####
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
+export CUDA_HOME=/usr/local/cuda
+export PYTHONPATH=$MARS_LIB_PATH:$MARS_PATH
+
+KEY=${1}
+WORKER_IDX=${2}
+
+/usr/local/bin/python3.7 -m mars.MLAlgRecommender ${KEY} ${WORKER_IDX}
